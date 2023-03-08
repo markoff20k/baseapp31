7d51c1e33c096698a9e750f2c6c0012bd1902d49
Jeferson Martin de Araujo <jmartin20k@gmail.com>
2023-03-08 08:08:25 -0300

    v2

--- web/webpack/production.ts
+++ web/webpack/production.ts
@@ -52,7 +52,20 @@ const config = merge(commonConfig, {
         rules: [
             {
                 test: /\.(css|sass|scss|pcss)$/,
-                use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader', 'postcss-loader'],
+                use: [
+                    'style-loader',
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: true,
+                            reloadAll: true,
+                        },
+                    },
+                    'cache-loader',
+                    'css-loader',
+                    'sass-loader',
+                    'postcss-loader',
+                ],
             },
             {
                 test: /\.(tsx|ts)?$/,
@@ -82,6 +95,8 @@ const config = merge(commonConfig, {
                     },
                 }]
             },
+
+            { test: /\.(js|jsx)?$/, use: ["babel-loader"], exclude: /node_modules/, },
         ],
     },
     stats: {
